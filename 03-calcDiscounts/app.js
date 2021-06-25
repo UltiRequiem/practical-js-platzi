68da1e0b3a94ccbeaedba7f68dd7585ed06860e6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-25 13:44:40 -0500

    :art: Update

---
+++ 03-calcDiscounts/app.js
@@ -0,0 +1,12 @@
+const calcDescount = (price, descount) => (price * (100 - descount)) / 100;
+
+const onClickButtonPriceDiscount = () => {
+  const descountedPrice = calcDescount(
+    document.getElementById('price').value,
+    document.getElementById('discount').value
+  );
+
+  document.getElementById(
+    'result'
+  ).innerText = `El precio con descuento son: $${descountedPrice}`;
+};
