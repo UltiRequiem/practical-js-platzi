6e8afc75a5069c50e600e85e0010c868f3f935f1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-24 22:20:11 -0500

    :art: Update

--- 02-code/figures.js
+++ 02-code/figures.js
@@ -1,47 +1,27 @@
-console.group('Cuadrados');
-
-const perimetroCuadrado = (lado) => lado * 4;
-
-const areaCuadrado = (lado) => lado * lado;
-
-console.groupEnd();
-
-console.group('Triángulos');
-
-const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
-
-const areaTriangulo = (base, altura) => (base * altura) / 2;
-
-console.log(`El área del triángulo es: ${areaTriangulo}cmˆ2`);
-
-console.groupEnd();
-
-console.group('Círculos');
-
-const diametroCirculo = (radio) => radio * 2;
-
+// Constants
 const { PI } = Math;
 
-console.log(`PI es: ${PI}`);
+// Functions
+const perimeterSquare = (l) => l * 4;
 
-const perimetroCirculo = (radio) => diametroCirculo(radio) * PI;
+const areaSquare = (l) => l * l;
 
-const areaCirculo = (radio) => radio * radio * PI;
+const perimeterTriangule = (lOne, lTwo, b) => lOne + lTwo + b;
 
-console.groupEnd();
+const areaTriangle = (b, h) => (b * h) / 2;
 
-const calcularPerimetroCuadrado = () => {
-  const input = document.getElementById('InputCuadrado');
-  const { value } = input;
+const circuleDiameter = (r) => r * 2;
 
-  const perimetro = perimetroCuadrado(value);
-  alert(perimetro);
+const calculatePerimeterSquare = () => {
+  // eslint-disable-next-line no-alert
+  alert(perimeterSquare(document.getElementById('InputCuadrado').value));
 };
 
-const calcularAreaCuadrado = () => {
-  const input = document.getElementById('InputCuadrado');
-  const { value } = input;
-
-  const area = areaCuadrado(value);
-  alert(area);
+const calculateSquareArea = () => {
+  // eslint-disable-next-line no-alert
+  alert(areaSquare(document.getElementById('InputCuadrado').value));
 };
+
+const perimeterCircule = (r) => circuleDiameter(r) * PI;
+
+const areaCircule = (r) => r * r * PI;
